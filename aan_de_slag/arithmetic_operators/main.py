274f7f93e4cc864134985a69002dbbbcf14e4053
bertvanderwoerd <>
2025-12-17 21:29:22 +0000

    Arithmetic Operators

---
+++ aan_de_slag/arithmetic_operators/main.py
@@ -0,0 +1,7 @@
+# Calculate addition and format to 2 decimals
+result_add = 5.42 + 4.39
+print(f"{result_add:.2f}")
+
+# Calculate subtraction and format to 2 decimals
+result_sub = 12.30 - 5.50
+print(f"{result_sub:.2f}")
